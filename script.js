24387acead3b649de8d0eb40f4c9e4b399634514
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-16 14:09:43 +0200

    Add if statement to check for:  1.the input is less than or equal to 100  2.the input is a number.

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ button.addEventListener("click" , () => {
     container.textContent = "";
     let sqaureNumber = parseInt(prompt("How many sqares per side do you like?"));
 
+    if(sqaureNumber > 100){
+        window.alert("Maximum number is 100");
+    }else if (isNaN(sqaureNumber) == true){
+        window.alert("You have to enter a number");
+    }else{
+
     for (i = 0; i < sqaureNumber*sqaureNumber ; i++){
         let square = document.createElement('div');
         container.appendChild(square);
@@ -16,4 +22,5 @@ button.addEventListener("click" , () => {
         grid-template-columns: repeat(${sqaureNumber}, 1fr [col-start]);
         grid-template-rows: repeat(${sqaureNumber}, 1fr [row-start]);
     `
+    }
 })
