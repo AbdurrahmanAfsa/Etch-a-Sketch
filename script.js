1b3fff3202b2b3500a4ce43b1b8d23368ac2e4e5
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-16 12:52:23 +0200

    Add let before square. Add event listener to squaare to change its background color when the mouse hovers over it.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let container = document.querySelector('.container');
 
 for (i = 0; i < 256 ; i++){
-    square = document.createElement('div');
+    let square = document.createElement('div');
     container.appendChild(square);
+    square.addEventListener('mouseover' , ()=>{
+        square.style.cssText = "background-color: black;"
+    });
 }
