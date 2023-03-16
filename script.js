7a8054e361b1f3dfd2f3ae97911b1d60ba038937
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-16 13:38:17 +0200

    Put button in a variable.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let container = document.querySelector('.container');
+let button = document.querySelector('.button');
 
-for (i = 0; i < 256 ; i++){
-    let square = document.createElement('div');
-    container.appendChild(square);
-    square.addEventListener('mouseover' , ()=>{
-        square.style.cssText = "background-color: black;"
-    });
-}
+    for (i = 0; i < 256 ; i++){
+        let square = document.createElement('div');
+        container.appendChild(square);
+        square.addEventListener('mouseover' , ()=>{
+            square.style.cssText = "background-color: black;"
+        });
+    }
