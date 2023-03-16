523c86c50ed16510512447a30d1759d705679dd1
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-16 13:49:43 +0200

    - Add eventListener to button. - Add squareNumber variable to equal a prompt asking the user to   type how many sqares per sides they want. - Put the for loop inside the eventListener.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let container = document.querySelector('.container');
 let button = document.querySelector('.button');
 
-    for (i = 0; i < 256 ; i++){
+button.addEventListener("click" , () => {
+    let sqaureNumber = parseInt(prompt("How many sqares per side do you like?"));
+
+    for (i = 0; i < sqaureNumber*sqaureNumber ; i++){
         let square = document.createElement('div');
         container.appendChild(square);
         square.addEventListener('mouseover' , ()=>{
             square.style.cssText = "background-color: black;"
         });
     }
+})
